00ffc4fa052889d281f071372326a45263396665
LUIS MORALES <lic.morales.luis@gmail.com>
2026-09-23 22:53:30 -0400

    Creando proyecto MAUI y sus componentes
    Creando proyecto MAUI y sus componentes

---
+++ App_Mobile_Cooperativa/App_Mobile_Cooperativa/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace App_Mobile_Cooperativa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ App_Mobile_Cooperativa/App_Mobile_Cooperativa/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace App_Mobile_Cooperativa
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
